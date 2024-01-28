cd7d8d2a0df3def3383eda557c59c4f44c3bf53a
JamesBellew <11jamesbellew@gmail.com>
2024-01-28 19:24:03 +0000

    1 algo for today, piss easy bruh

---
+++ Arrays/ConsistentStrings.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        x = list(allowed)
+        output = 0
+        for i in range(len(words)):
+            print(words[i])
+            count =0
+            for j in range(len(words[i])):
+                print(words[i][j])
+                if words[i][j] in x:
+                    count +=1
+                    if count == len(words[i]):
+                        output +=1
+        return output
+# Testing the function
+solution = Solution()
+result = solution.countConsistentStrings("cad",["cc","acd","b","ba","bac","bad","ac","d"])
+print(result)
